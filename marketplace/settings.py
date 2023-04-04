862f20d2e2e9230be51b1b70c0da170057c7e7a7
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-03 19:00:39 -1100

    Add decouple package

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 """
 Django settings for marketplace project.
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%_)7qwa%1os-gpl@mvtv0#%@f-blz44)1@v3m*(#1-pvcpk7(o'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
